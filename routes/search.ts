1bb35bd9d1a8c041d5a5cfe57adde39be471885f
Corentin <corentinrubio63@gmail.com>
2024-02-09 16:46:56 +0100

    Re-Re-Fix : Injection SQL

--- routes/search.ts
+++ routes/search.ts
@@ -20,12 +20,16 @@ module.exports = function searchProducts () {
   return (req: Request, res: Response, next: NextFunction) => {
     let criteria: any = req.query.q === 'undefined' ? '' : req.query.q ?? ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
-    const onlyLettersPattern = /^[A-Za-z]+$/;
+
+    if (criteria.length > 1) {
+      const onlyLettersPattern = /^[A-Za-z]+$/;
 
 
-    if(!criteria.match(onlyLettersPattern)){
-      return res.status(400).json({ err: "No special characters and no numbers, please!"})
+      if(!criteria.match(onlyLettersPattern)){
+        return res.status(400).json({ err: "No special characters and no numbers, please!"})
+      }
     }
+    
 
     models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`) // vuln-code-snippet vuln-line unionSqlInjectionChallenge dbSchemaChallenge
       .then(([products]: any) => {
